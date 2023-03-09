07b7b382841843c044c49c2c14c07325bbec97a2
mayur-said <67707663+mayur-said@users.noreply.github.com>
2023-03-09 12:26:02 +0000

    Adding Files to GitHub

---
+++ ETL-PySpark/etl_pyspark.py
@@ -0,0 +1,160 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_timestamp
+import os
+
+def create_spark_session():
+    spark = SparkSession.builder \
+        .master("yarn") \
+        .appName('new-york-taxi-pipeline') \
+        .getOrCreate()
+    return spark
+
+
+if __name__ == '__main__':
+    spark = create_spark_session()
+
+    #Get the environment variables
+    TRIP_DATA_PATH = os.environ.get('TRIP_DATA_PATH', 'gs://new-york-taxi-data-bucket/trip_data/trip_data_test.csv')
+    FARE_DATA_PATH = os.environ.get('FARE_DATA_PATH', 'gs://new-york-taxi-data-bucket/fare_data/fare_data_test.csv')
+    BUCKET_NAME = os.environ.get('BUCKET', 'new-york-taxi-tutorial-project-temporary-bucket')
+    BQ_DATASET = os.environ.get('BQ_DATASET', 'new_york_taxi_data_bq')
+    BQ_TABLE = os.environ.get('BQ_TABLE', 'new_york_taxi_data_cleaned')
+    
+    #Extraction phase
+    ##read data from cloud storage
+    trip_data = spark.read.option("header", True).option("inferSchema", True).csv(TRIP_DATA_PATH)
+    fare_data = spark.read.option("header", True).option("inferSchema", True).csv(FARE_DATA_PATH)
+
+    
+    #Transformation Phase
+    #remove leading white space from column names
+    #trip data
+    old_column_names = [
+        ' hack_license',
+        ' vendor_id',
+        ' rate_code',
+        ' store_and_fwd_flag',
+        ' pickup_datetime',
+        ' dropoff_datetime',
+        ' passenger_count',
+        ' trip_time_in_secs',
+        ' trip_distance',
+        ' pickup_longitude',
+        ' pickup_latitude',
+        ' dropoff_longitude',
+        ' dropoff_latitude'
+        ]
+    
+    new_column_names = [
+        'hack_license',
+        'vendor_id',
+        'rate_code',
+        'store_and_fwd_flag',
+        'pickup_datetime',
+        'dropoff_datetime',
+        'passenger_count',
+        'trip_time_in_secs',
+        'trip_distance',
+        'pickup_longitude',
+        'pickup_latitude',
+        'dropoff_longitude',
+        'dropoff_latitude'
+        ]
+    
+    for i in range(len(old_column_names)):
+        trip_data = trip_data.withColumnRenamed(old_column_names[i], new_column_names[i])
+
+    #fare data
+    old_column_names = [
+        ' hack_license',
+        ' vendor_id',
+        ' pickup_datetime',
+        ' payment_type',
+        ' fare_amount',
+        ' surcharge',
+        ' mta_tax',
+        ' tip_amount',
+        ' tolls_amount',
+        ' total_amount'
+        ]
+    
+    new_column_names = [
+        'hack_license',
+        'vendor_id',
+        'pickup_datetime',
+        'payment_type',
+        'fare_amount',
+        'surcharge',
+        'mta_tax',
+        'tip_amount',
+        'tolls_amount',
+        'total_amount'
+        ]
+    
+    for i in range(len(old_column_names)):
+        fare_data = fare_data.withColumnRenamed(old_column_names[i], new_column_names[i])
+
+    #convert string to timestamp
+    trip_data = trip_data.withColumn('pickup_datetime', to_timestamp('pickup_datetime')) \
+            .withColumn('dropoff_datetime', to_timestamp('dropoff_datetime'))
+    
+
+    #Join Trip Data and Fare Data
+    trip_data.createOrReplaceTempView("trip_data")
+    fare_data.createOrReplaceTempView("fare_data")
+
+    final_df = spark.sql('''
+    select
+        a.id, 
+        a.medallion,
+        a.hack_license,
+        a.vendor_id,
+        a.rate_code,
+        a.pickup_datetime,
+        a.dropoff_datetime,
+        a.passenger_count,
+        a.trip_time_in_secs,
+        a.trip_distance,
+        a.pickup_longitude,
+        a.pickup_latitude,
+        a.dropoff_longitude,
+        a.dropoff_latitude,
+        b.payment_type,
+        b.fare_amount,
+        b.surcharge,
+        b.mta_tax,
+        b.tip_amount,
+        b.tolls_amount,
+        b.total_amount
+    from trip_data as a 
+        left join fare_data as b
+            on a.id = b.id
+    '''
+                         )
+    
+    ## data validation and data accuracy
+    # drop null values in 'passenger_count', 'trip_time_in_secs', 'trip_distance', 'fare_amount'
+    final_df = final_df.na.drop(subset=[
+    'passenger_count',
+    'trip_time_in_secs',
+    'trip_distance',
+    'fare_amount'
+    ])
+
+    # 'passenger_count', 'trip_time_in_secs', 'trip_distance', 'fare_amount' must be greater than 0
+    final_df = final_df.filter((final_df.passenger_count > 0) & \
+                           (final_df.trip_time_in_secs > 0) & \
+                           (final_df.trip_distance > 0) & \
+                           (final_df.fare_amount > 0)
+                          )
+
+
+    #Loading Phase
+    # Saving the final to BigQuery
+    # Use the Cloud Storage bucket for temporary BigQuery export data used by the connector.
+    spark.conf.set('temporaryGcsBucket', BUCKET_NAME)
+
+    final_df.write.format('bigquery') \
+    .option('table', f'{BQ_DATASET}.{BQ_TABLE}') \
+    .mode('append') \
+    .save()
---
+++ airflow dag/new-york-taxi-dag.py
@@ -0,0 +1,107 @@
+from airflow import DAG
+import os
+
+#gcp operators
+from airflow.providers.google.cloud.sensors import gcs
+
+#dataproc 
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocDeleteClusterOperator,
+    DataprocSubmitJobOperator,
+    ClusterGenerator
+)
+
+#other operators
+from airflow.utils.dates import days_ago
+
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "admin@localhost.com",
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5)
+}
+
+## Environment Variables
+CLUSTER_NAME = os.environ.get('CLUSTER_NAME', 'new-york-taxi-dataproc-cluster')
+GCP_PROJECT_ID = os.environ.get('GCP_PROJECT_ID', "new-york-taxi-tutorial-project")
+REGION = os.environ.get('REGION', 'europe-west1')
+ZONE = os.environ.get('ZONE', "europe-west1-d")
+
+#check operator
+BUCKET_NAME = os.environ.get('BUCKET_NAME', "new-york-taxi-data-bucket")
+TRIP_DATA_FILE_NAME_PREFIX = os.environ.get('TRIP_DATA_FILE_NAME_PREFIX', "trip_data/trip_data")
+FARE_DATA_FILE_NAME_PREFIX = os.environ.get('FARE_DATA_FILE_NAME_PREFIX', "fare_data/fare_data")
+
+#Others
+TEMP_BUCKET = os.environ.get('TEMP_BUCKET', "new-york-taxi-tutorial-project-temporary-bucket")
+
+
+CLUSTER_GENERATOR_CONFIG = ClusterGenerator(
+    project_id=GCP_PROJECT_ID,
+    zone=ZONE,
+    master_machine_type="n2-standard-2",
+    worker_machine_type="n2-standard-2",
+    num_workers=2,
+    worker_disk_size=300,
+    master_disk_size=300,
+    storage_bucket=TEMP_BUCKET,
+).make()
+
+PYSPARK_JOB = {
+    "reference": {"project_id": GCP_PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {
+    "main_python_file_uri": "gs://new-york-taxi-etl-pipeline-code/etl_pyspark.py",
+    "jar_file_uris": [
+    "gs://spark-lib/bigquery/spark-3.1-bigquery-0.28.0-preview.jar"
+    ]
+    },
+}
+
+
+with DAG(dag_id="new-york-taxi-pipeline", schedule_interval="@monthly", default_args=default_args, start_date=days_ago(2), tags=['new-york-taxi'], catchup=False) as dag:
+
+    check_trip_data_file = gcs.GCSObjectsWithPrefixExistenceSensor(
+        task_id = "check_trip_data_file",
+        bucket = BUCKET_NAME,
+        prefix = TRIP_DATA_FILE_NAME_PREFIX,
+        google_cloud_conn_id = 'google_cloud_storage_default'
+    )
+
+    check_fare_data_file = gcs.GCSObjectsWithPrefixExistenceSensor(
+        task_id = "check_fare_data_file",
+        bucket = BUCKET_NAME,
+        prefix = FARE_DATA_FILE_NAME_PREFIX,
+        google_cloud_conn_id = 'google_cloud_storage_default'
+    )
+
+    create_dataproc_cluster = DataprocCreateClusterOperator(
+        task_id="create_dataproc_cluster",
+        cluster_name=CLUSTER_NAME,
+        project_id=GCP_PROJECT_ID,
+        region=REGION,
+        cluster_config=CLUSTER_GENERATOR_CONFIG,
+    )
+    submit_pyspark_job = DataprocSubmitJobOperator(
+    task_id="submit_pyspark_job", job=PYSPARK_JOB, region=REGION, project_id=GCP_PROJECT_ID)
+
+    delete_dataproc_cluster = DataprocDeleteClusterOperator(
+    task_id="delete_dataproc_cluster",
+    project_id=GCP_PROJECT_ID,
+    cluster_name=CLUSTER_NAME,
+    region=REGION,
+    )
+
+[check_trip_data_file, check_fare_data_file] >> create_dataproc_cluster >> submit_pyspark_job >> delete_dataproc_cluster
+
+
+
+    
+
+    
+
